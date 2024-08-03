8ee40a4733154d84cb41c04815a75d7b2a09e1a9
Ryan Matthews <mhaddy@gmail.com>
2024-08-02 18:05:53 -0700

    Replace picamera with picamerax
    Picamera is in legacy mode. Replace with picamerax that is more active

--- imutils/video/pivideostream.py
+++ imutils/video/pivideostream.py
@@ -1,6 +1,6 @@
 # import the necessary packages
-from picamera.array import PiRGBArray
-from picamera import PiCamera
+from picamerax.array import PiRGBArray
+from picamerax import PiCamera
 from threading import Thread
 import cv2
 
